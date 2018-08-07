7698295911f8765d90918c5c661acf5e6d8885a9
LIYOU ZHOU <leothemagnificent@gmail.com>
2018-08-07 23:39:20 +0100

    Build out little bit more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import os.path
 import tensorflow as tf
 import helper
@@ -55,22 +56,33 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    decoder_input = vgg_layer7_out
 
-    pool3_out_scaled = tf.multiply(pool3_out, 0.0001, name=‘pool3_out_scaled’)
-    pool4_out_scaled = tf.multiply(pool4_out, 0.01, name=‘pool4_out_scaled’)
+    # scale the jump layers
+    vgg_layer3_out_scaled = tf.multiply(vgg_layer3_out, 0.0001, name="vgg_layer3_out_scaled")
+    vgg_layer4_out_scaled = tf.multiply(vgg_layer4_out, 0.01, name="vgg_layer4_out_scaled")
 
-    # conv1x1 = 
-    output = tf.layers.conv2d_transpose(decoder_input, num_classes, 4, strides=(2, 2))
+    # conv1x1
+    vgg_layer7_conv1x1_out = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, 1, padding="SAME")
+    vgg_layer4_conv1x1_out = tf.layers.conv2d(vgg_layer4_out_scaled, num_classes, 1, 1, padding="SAME")
+    vgg_layer3_conv1x1_out = tf.layers.conv2d(vgg_layer3_out_scaled, num_classes, 1, 1, padding="SAME")
 
-    # skip connections
-    decoder_layer1 = tf.add(decoder_input, vgg_layer4_out)
-    decoder_layer1_out = tf.layers.conv2d_transpose(decoder_layer1, num_classes, 4, strides=(2, 2))
+    # scale by 2
+    decoder_layer1_out = tf.layers.conv2d_transpose(vgg_layer7_conv1x1_out, num_classes, 4, 2, padding="SAME")
 
-    decoder_layer2 = tf.add(decoder_layer1_out, vgg_layer3_out)
-    decoder_layer2_out = tf.layers.conv2d_transpose(decoder_layer2, num_classes, 16, strides=(8, 8))
+    # add jump connections
+    decoder_layer1_out = tf.add(decoder_layer1_out, vgg_layer4_conv1x1_out)
 
-    return None
+    # scale by 2
+    decoder_layer2_out = tf.layers.conv2d_transpose(decoder_layer1_out, num_classes, 4, 2)
+
+    # add jump connections
+    decoder_layer2_out = tf.add(decoder_layer2_out, vgg_layer3_conv1x1_out)
+
+    # scale by 8
+    decoder_layer3_out = tf.layers.conv2d_transpose(decoder_layer2_out, num_classes, 16, 8)
+    print(tf.shape(decoder_layer3_out))
+
+    return decoder_layer3_out
 tests.test_layers(layers)
 
 
@@ -110,7 +122,9 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     """
     # TODO: Implement function
     for epoch in range(epochs):
-        for images, labels in get_batches_fn(batch_size)
+        print("epoch:", epochs)
+        for images, labels in get_batches_fn(batch_size):
+            print("images:", images.shape, "labels:", labels.shape)
             sess.run(train_op, feed_dict={input_image: images, correct_label: labels, keep_prob: 0.045, learning_rate:0.0001})
 tests.test_train_nn(train_nn)
 
@@ -123,7 +137,10 @@ def run():
     epochs = 1
     batch_size = 32
     rate = 0.001
-    get_batches_fn = gen_batch_function(data_dir, image_shape)
+    # Path to vgg model
+    vgg_path = os.path.join(data_dir, 'vgg')
+    # Create function to get batches
+    get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
@@ -133,23 +150,30 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+    # OPTIONAL: Augment Images for better results
+    #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+
     with tf.Session() as sess:
-        # Path to vgg model
-        vgg_path = os.path.join(data_dir, 'vgg')
-        # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
-
-        # OPTIONAL: Augment Images for better results
-        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
-
         # TODO: Build NN using load_vgg, layers, and optimize function
-        image_input, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
-        layers_output = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        layers_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
+
+        # TF placeholders
+        correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
 
         # TODO: Train NN using the train_nn function
-        training_operation = optimizer.minimize(loss_operation)
-        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
-
+        logits, train_op, loss_operation = optimize(layers_output, correct_label, learning_rate, num_classes)
+        train_nn(sess,
+                 epochs,
+                 batch_size,
+                 get_batches_fn,
+                 train_op,
+                 loss_operation,
+                 input_image,
+                 correct_label,
+                 keep_prob,
+                 learning_rate)
 
 
         # TODO: Save inference data using helper.save_inference_samples
