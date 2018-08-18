1f47e8873256439b03857f09fe9a28775b8510ba
LIYOU ZHOU <leothemagnificent@gmail.com>
2018-08-18 22:39:53 +0100

    Clean up code

--- main.py
+++ main.py
@@ -26,8 +26,7 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+    # Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -125,7 +124,6 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
     session_id = str(uuid.uuid4())[:6]
     for epoch in range(epochs):
         sess.run(tf.global_variables_initializer())
@@ -152,7 +150,7 @@ def run():
     image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
-    epochs = 1
+    epochs = 4
     batch_size = 2
     rate = 0.001
     # Path to vgg model
@@ -164,13 +162,6 @@ def run():
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
 
-    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
-    # You'll need a GPU with at least 10 teraFLOPS to train on.
-    #  https://www.cityscapes-dataset.com/
-
-    # OPTIONAL: Augment Images for better results
-    #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
-
     with tf.Session() as sess:
         # TODO: Build NN using load_vgg, layers, and optimize function
         input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
@@ -180,7 +171,7 @@ def run():
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
         learning_rate = tf.placeholder(tf.float32, name='learning_rate')
 
-        # TODO: Train NN using the train_nn function
+        # Train NN using the train_nn function
         logits, train_op, loss_operation = optimize(layers_output, correct_label, learning_rate, num_classes)
         train_nn(sess,
                  epochs,
@@ -193,11 +184,8 @@ def run():
                  keep_prob,
                  learning_rate)
 
-        # TODO: Save inference data using helper.save_inference_samples
+        # Save inference data using helper.save_inference_samples
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
-
-        # OPTIONAL: Apply the trained model to a video
-
 
 if __name__ == '__main__':
     run()
